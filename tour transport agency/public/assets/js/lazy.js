1c62cb80853a4c75b10f378aaddf9522c7f9446c
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-03-22 22:37:45 +0000

    project moved

--- tour transport agency/public/assets/js/lazy.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll("img").forEach(img => {
-        if (!img.hasAttribute("loading")) {
-            img.setAttribute("loading", "lazy");
-        }
-    });
-});
